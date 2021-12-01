5f9c996bb599693aeedd5fb6c2c56f76dadfa13d
tobiasp89tp@gmai.com <tobiasp89tp@gmail.com>
2021-12-01 16:05:29 +0100

    Icon Fixed

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,49 +1,44 @@
 import React from "react";
 import "../css/site-skeleton.css";
-import { RiBarChartFill } from "react-icons/ri";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import * as AiIcons from "react-icons/ri";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Sidebar(){
-    return(
-        <main>
-            <Router>
-                <div className="sidebar">
-                    <nav>
-                        <ul>
-                            <li>
-                                <RiBarChartFill/>
-                                <Link to="/"> Summary</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Budget</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Expense</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Income</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Saving-goal</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Social</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Subscribe</Link>
-                            </li>
-                            <li>
-                                <Link to="/">Sign-out</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </Router>
-        </main>
-    )
+export default function Sidebar() {
+  return (
+    <main>
+      <Router>
+        <div className="sidebar">
+          <nav>
+            <ul>
+              <li>
+                <AiIcons.RiBarChartFill />
+                <Link to="/"> Summary</Link>
+              </li>
+              <li>
+                <Link to="/">Budget</Link>
+              </li>
+              <li>
+                <Link to="/">Expense</Link>
+              </li>
+              <li>
+                <Link to="/">Income</Link>
+              </li>
+              <li>
+                <Link to="/">Saving-goal</Link>
+              </li>
+              <li>
+                <Link to="/">Social</Link>
+              </li>
+              <li>
+                <Link to="/">Subscribe</Link>
+              </li>
+              <li>
+                <Link to="/">Sign-out</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
+    </main>
+  );
 }
