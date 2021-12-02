9ee3853d9c3bc4d8bf7e5c420257ca01053091f2
Martin <biszczak@live.se>
2021-12-02 15:45:40 +0100

    sidebar icons

--- src/App.js
+++ src/App.js
@@ -5,5 +5,4 @@ function App() {
     <Sidebar></Sidebar>
   );
 }
-
 export default App;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../css/site-skeleton.css";
 import * as AiIcons from "react-icons/ri";
+import * as AiIcons2 from "react-icons/go";
+import * as AiIcons3 from "react-icons/gr";
+import * as AiIcons4 from "react-icons/fa";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Sidebar() {
@@ -11,28 +14,35 @@ export default function Sidebar() {
           <nav>
             <ul>
               <li>
-                <AiIcons.RiBarChartFill />
+                <AiIcons.RiBarChartFill/>
                 <Link to="/"> Summary</Link>
               </li>
               <li>
+                <AiIcons.RiCalendar2Fill/>
                 <Link to="/">Budget</Link>
               </li>
               <li>
+                <AiIcons2.GoCreditCard/>
                 <Link to="/">Expense</Link>
               </li>
               <li>
+                <AiIcons3.GrMoney/>
                 <Link to="/">Income</Link>
               </li>
               <li>
+                <AiIcons.RiRocketLine/>
                 <Link to="/">Saving-goal</Link>
               </li>
               <li>
+                <AiIcons4.FaUserFriends/>
                 <Link to="/">Social</Link>
               </li>
               <li>
+                <AiIcons2.GoCheck/>
                 <Link to="/">Subscribe</Link>
               </li>
               <li>
+                <AiIcons2.GoSignOut/>
                 <Link to="/">Sign-out</Link>
               </li>
             </ul>
